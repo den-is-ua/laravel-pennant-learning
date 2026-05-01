ee5c8517e5274a7e3dd53ef0c0b649a6b06a32d5
admin <admin@admins-MacBook-Pro.local>
2026-05-01 08:08:34 +0300

    prepare policy

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -11,7 +11,7 @@ class StorePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return $this->user() !== null;
+        return true;
     }
 
     /**
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Policies\PostPolicy;
 use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Auth;
 
 #[Fillable(['title', 'body'])]
+#[UsePolicy(PostPolicy::class)]
 class Post extends Model
 {
     public function user(): BelongsTo
