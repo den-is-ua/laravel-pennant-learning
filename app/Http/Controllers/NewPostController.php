404279422fe24344f6fa665929dcd162970981e0
admin <admin@admins-MacBook-Pro.local>
2026-05-04 09:57:54 +0300

    add policy

--- app/Http/Controllers/NewPostController.php
+++ app/Http/Controllers/NewPostController.php
@@ -6,12 +6,15 @@ use App\Http\Requests\StoreNewPostRequest;
 use App\Http\Requests\UpdateNewPostRequest;
 use App\Models\NewPost;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Attributes\Controllers\Authorize;
 use Illuminate\View\View;
 
 use function auth;
 
 class NewPostController extends Controller
-{
+{ 
+ 
+    #[Authorize('viewAny', NewPost::class)]
     public function index(): View
     {
         return view('new-posts.index', [
@@ -21,11 +24,13 @@ class NewPostController extends Controller
         ]);
     }
 
+    #[Authorize('create', NewPost::class)]
     public function create(): View
     {
         return view('new-posts.create');
     }
 
+    #[Authorize('create', NewPost::class)]
     public function store(StoreNewPostRequest $request): RedirectResponse
     {
         $newPost = auth()->user()->newPosts()->create([...$request->validated()]);
@@ -33,16 +38,19 @@ class NewPostController extends Controller
         return redirect()->route('new-posts.show', $newPost)->with('status', 'new-post-created');
     }
 
+    #[Authorize('view', 'new_post')]
     public function show(NewPost $newPost): View
     {
         return view('new-posts.show', compact('newPost'));
     }
 
+    #[Authorize('update', 'new_post')]
     public function edit(NewPost $newPost): View
     {
         return view('new-posts.edit', compact('newPost'));
     }
 
+    #[Authorize('update', 'new_post')]
     public function update(UpdateNewPostRequest $request, NewPost $newPost): RedirectResponse
     {
         $newPost->update($request->validated());
@@ -50,6 +58,7 @@ class NewPostController extends Controller
         return redirect()->route('new-posts.show', $newPost)->with('status', 'new-post-updated');
     }
 
+    #[Authorize('delete', 'new_post')]
     public function destroy(NewPost $newPost): RedirectResponse
     {
         $newPost->delete();
